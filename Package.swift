68badc662b2428ac0d1e6bf9601c3f44bf504ce7
CodingIran <codingiran@gmail.com>
2025-01-24 16:52:56 +0800

    support more platforms

--- Package.swift
+++ Package.swift
@@ -1,15 +1,17 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "AnyCodable",
-    platforms:  [
-        .iOS(.v9),
-        .macOS(.v10_10),
-        .tvOS(.v9),
-        .watchOS(.v2),
+    platforms: [
+        .iOS(.v12),
+        .macOS(.v10_13),
+        .macCatalyst(.v13),
+        .tvOS(.v12),
+        .watchOS(.v4),
+        .visionOS(.v1),
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
@@ -27,7 +29,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "AnyCodable",
-            dependencies: []
+            dependencies: [],
+            resources: [.copy("Resources/PrivacyInfo.xcprivacy")]
         ),
         .testTarget(
             name: "AnyCodableTests",
